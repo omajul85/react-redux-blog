02cf62426d13dbd0d1d53ec33d2334ee9dd3153b
Omar Alvarez <omajul2@hotmail.com>
2017-07-07 16:35:27 +0100

    Form validation

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -7,6 +7,7 @@ class PostsNew extends Component {
       <div className="form-group">
         <label>{field.label}</label>
         <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
       </div>
     )
   }
@@ -15,13 +16,35 @@ class PostsNew extends Component {
     return (
       <form>
         <Field name="title" label="Title" component={this.renderField} />
-        <Field name="tags" label="Tags" component={this.renderField} />
+        <Field name="categories" label="Categories" component={this.renderField} />
         <Field name="content" label="Content" component={this.renderField} />
       </form>
     );
   }
 }
 
+// values -> { title: 'foo', categories: 'tito, tete', content: 'La la la'}
+function validate(values) {
+  const errors = {};
+
+  // Validate the input from values
+  if (!values.title) {
+    errors.title = "Enter a title"
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories"
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content"
+  }
+
+  // if errors if empty, the form is fine to submit. In case of errors, the form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm' //this is kind of a namespace (unique) for the form
 })(PostsNew);
